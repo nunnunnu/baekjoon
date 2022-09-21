9c198339ce18d77a30eaf04739b5b6ea2138d0ac
jinhee park <bis04199@gmail.com>
2022-09-21 21:52:21 +0900

    A

---
+++ src/L2/bj9498.java
@@ -0,0 +1,17 @@
+package L2;
+
+import java.util.Scanner;
+
+public class bj9498 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int score = s.nextInt();
+		if(score>=90) System.out.println("A");
+		else if(score>=80) System.out.println("B");
+		else if(score>=70) System.out.println("C");
+		else if(score>=60) System.out.println("D");
+		else System.out.println("F");
+	}
+
+}
