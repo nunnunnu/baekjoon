f1d9ec6c58298c868fe6f40d7b39171e09f93019
jinhee park <bis04199@gmail.com>
2022-09-23 11:05:49 +0900

    *

--- src/L3/bj11021.java
+++ src/L3/bj11021.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class bj11021 {
 
 	public static void main(String[] args) {
+//		Scanner s = new Scanner(System.in);
+//		int t=s.nextInt();
+//		for(int i=1;i<=t;i++) {
+//			int a=s.nextInt();
+//			int b=s.nextInt();
+//			System.out.println("Case #"+i+": "+(a+b));
+		
 		Scanner s = new Scanner(System.in);
 		int t=s.nextInt();
 		for(int i=1;i<=t;i++) {
 			int a=s.nextInt();
 			int b=s.nextInt();
-			System.out.println("Case #"+i+": "+(a+b));
+			System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
 		}
 	}
 
---
+++ src/L3/bj2438.java
@@ -0,0 +1,19 @@
+package L3;
+
+import java.util.Scanner;
+
+public class bj2438 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t=s.nextInt();
+		
+		for(int i=0;i<t;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
