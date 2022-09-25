d72efd02c7a088bba9f961295c9545de8a474d27
jinhee park <bis04199@gmail.com>
2022-09-25 22:21:24 +0900

    ASCII

---
+++ src/L6/bj11654.java
@@ -0,0 +1,19 @@
+package L6;
+
+public class bj11654 {
+
+	public static void main(String[] args) throws Exception {
+//		Scanner s = new Scanner(System.in);
+//		int ascii = s.next().charAt(0);
+//		System.out.println(ascii);
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int ascii = (char) br.read();
+//		System.out.println(ascii);
+		
+		int a = System.in.read();
+		System.out.print(a);
+		
+	}
+
+}
