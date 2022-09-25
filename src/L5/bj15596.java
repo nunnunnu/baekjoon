708ad69e67bd18c821689b42ba48c3378a69b00f
jinhee park <bis04199@gmail.com>
2022-09-25 12:33:33 +0900

    funtion

---
+++ src/L5/bj15596.java
@@ -0,0 +1,18 @@
+package L5;
+
+
+class Test{
+	static long sum(int a[]) {
+		long sum=0;
+		for(int i=0;i<a.length;i++) {
+			sum+=a[i];
+		}
+		return sum;
+	}
+}
+public class bj15596 {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/L5/bj4673.java
@@ -0,0 +1,24 @@
+package L5;
+
+public class bj4673 {
+	static int d(int n){
+		int sum =n;
+		while(n!=0) {
+			sum=sum+n%10; 
+			n=n/10; 
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		boolean check[] = new boolean[10001];
+		for(int i=1;i<10001;i++) {
+			int n = d(i);
+			
+			if(n<10001) {
+				check[n]=true;
+			}
+			if(check[i]!=true) System.out.println(i);
+		}
+	}
+
+}
